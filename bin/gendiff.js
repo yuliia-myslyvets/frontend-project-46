4d4ca2564683f4bcdd85cd763213c2a29306506f
Yuliia Myslyvets <yuliia@Yuliias-MacBook-Pro.local>
2023-02-03 14:14:33 +0200

    add option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ const { program } = require("commander");
 
 program
   .description("Compares two configuration files and shows a difference.")
-  .option("-V, --version", "output the version number");
+  .option("-V, --version", "output the version number")
+  .option("-f, --format <type>", "output format");
 
 program.parse();
