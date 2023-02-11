bb439711b73dbbe76737d8463bbe1e4d31695665
Yuliia Myslyvets <yuliia@Yuliias-MacBook-Pro.local>
2023-02-11 21:33:07 +0200

    fix test

--- __tests__/buildDiff.test.js
+++ __tests__/buildDiff.test.js
@@ -1,11 +1,11 @@
 import { test, expect } from "@jest/globals";
 import buildDiff from "../src/buildDiff.js";
 import { fileURLToPath } from "url";
-import { dirname, path } from "path";
+import path from "path";
 import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) =>
   path.join(__dirname, "..", "__fixtures__", filename);
